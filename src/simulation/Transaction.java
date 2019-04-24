eeba768642c981855132ecf4d2068466c45a907a
oquzhax <47812929+oquzhax@users.noreply.github.com>
2019-04-24 21:07:01 +0300

    Converter finished

--- src/simulation/Transaction.java
+++ src/simulation/Transaction.java
@@ -1,6 +1,7 @@
 package simulation;
 
 public class Transaction {
+	private String date; // date of the transaction
 	private int id; // unique id in [1,1000]
 	private ICustomer customer; // the request owner
 	private int occupation; // needed time for transaction
@@ -10,14 +11,16 @@ public class Transaction {
 	/**
 	 * Creates a transaction node with next transaction
 	 * 
+	 * @param date
 	 * @param id
 	 * @param customer
 	 * @param occupation
 	 * @param waiting
 	 * @param next
 	 */
-	public Transaction(int id, ICustomer customer, int occupation, int waiting, Transaction next) {
-		super();
+	public Transaction(String date, int id, ICustomer customer, int occupation, int waiting, Transaction next) {
+//		super();
+		this.date = date;
 		this.id = id;
 		this.customer = customer;
 		this.occupation = occupation;
@@ -33,15 +36,15 @@ public class Transaction {
 	 * @param occupation
 	 * @param waiting
 	 */
-	public Transaction(int id, ICustomer customer, int occupation, int waiting) {
-		this(id, customer, occupation, waiting, null);
+	public Transaction(String date, int id, ICustomer customer, int occupation, int waiting) {
+		this(date, id, customer, occupation, waiting, null);
 	}
 
 	/**
 	 * Creates an empty Transaction node
 	 */
 	public Transaction() {
-		this(-1, null, -1, -1, null);
+		this(null, -1, null, -1, -1, null);
 	}
 
 	@Override
@@ -117,6 +120,14 @@ public class Transaction {
 	 */
 	public void setNext(Transaction next) {
 		this.next = next;
+	}
+
+	public String getDate() {
+		return date;
+	}
+
+	public void setDate(String date) {
+		this.date = date;
 	}
 
 }
--- src/utility/Converter.java
+++ src/utility/Converter.java
@@ -1,10 +1,114 @@
 package utility;
 
+// A tool that reads the transaction.txt file and converts them to an array type of transaction
+
 import java.io.*;
-//TODO didnt start yet
 
-public class Converter<T> {
+import simulation.Corporate;
+import simulation.ICustomer;
+import simulation.Individual;
+import simulation.NonRegistered;
+import simulation.Transaction;
 
-	T[] lines;
+public class Converter {
+
+	private static Transaction[] transactions;
 	
+	/**
+	 * @return the transactions
+	 */
+	public static Transaction[] getTransactions() {
+		return transactions;
+	}
+
+	/**
+	 * @param transactions the transactions to set
+	 */
+	public static void setTransactions(Transaction[] transactions) {
+		Converter.transactions = transactions;
+	}
+
+	/**
+	 * reads file with buffered reader
+	 * 
+	 * @param filename
+	 * @return a file with type of BufferedReader
+	 */
+	private static BufferedReader readFile(String filename) {
+		BufferedReader file = null;
+		try {
+			file = new BufferedReader(new FileReader(filename));
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		}
+		return file;
+	}
+
+	/**
+	 * counts line number of the file
+	 * 
+	 * @param fileName
+	 * @return number of the lines
+	 */
+	private static int countLines(String fileName) {
+		BufferedReader file = readFile(fileName);
+		int lineCount = 0;
+		try {
+			while (file.readLine() != null) {
+				lineCount++;
+			}
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		return lineCount;
+	}
+
+	/**
+	 * main method for convert the transactions into our array
+	 * 
+	 * @param fileName
+	 * @return converted transaction array
+	 * @throws IOException
+	 */
+	public static Transaction[] toTransactions(String fileName) throws IOException {
+		BufferedReader file = readFile(fileName);
+		int lineCount = countLines(fileName);
+		transactions = new Transaction[lineCount]; // line number means transaction number
+		int waiting = 0; // waiting time initialize
+		// loop for every transaction
+		for (int i = 0; i < lineCount; i++) {
+			String singleLine = file.readLine();
+			String[] splitLine = singleLine.split(","); // splits by comma (by all attributes)
+			String date = splitLine[0];
+			int id = Integer.parseInt(splitLine[1]);
+			String customerType = splitLine[2];
+			ICustomer customer = null;
+			// determine the customer type
+			switch (customerType) { 
+			case "NON-REGISTERED":
+				customer = new NonRegistered();
+				break;
+			case "CORPORATE":
+				customer = new Corporate();
+				break;
+			case "INDIVIDUAL":
+				customer = new Individual();
+				break;
+			}
+			int occupation = Integer.parseInt(splitLine[3]);
+			// sums the waiting time
+			if (i == 0 || transactions[i - 1].getDate() != transactions[i].getDate()) { // TODO not sure about that
+				waiting = 0;
+			} else {
+				waiting += transactions[i - 1].getOccupation();
+			}
+			// bind the transaction nodes
+			if (i < lineCount) {
+				transactions[i - 1].setNext(transactions[i]);
+			}
+			transactions[i] = new Transaction(date, id, customer, occupation, waiting);
+		}
+		return transactions;
+	}
+
 }
