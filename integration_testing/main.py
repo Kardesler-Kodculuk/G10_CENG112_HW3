04e44338d7c245ebee91ce20393e6b49ce42d64f
Ege Emir Özkan <egeemirozkan24@gmail.com>
2019-04-25 19:16:26 +0300

    Done with the python

--- integration_testing/main.py
+++ integration_testing/main.py
@@ -1,15 +1,14 @@
 from typing import List, Dict, Tuple
-from simulation.simulation_os import position_test_files, execute_java_code, delete_file
+from simulation.simulation_os import position_test_files, delete_file
 from simulation.simulation_parts import load_transactions
 from simulation.simulation_classes import Transaction, TransactionQueue
-from simulation.simulation_parser import Parser
-
+from json import dumps
+from copy import deepcopy as copy
 
 class Main:
     def __init__(self, file_name: str):
         self.transactions: List[TransactionQueue] = []
         self.file_name: str = file_name
-        self.parsed_output: Tuple[List, Dict] = ([], {})
         self.stats = {}
 
     def ready_os(self) -> None:
@@ -17,7 +16,6 @@ class Main:
         Ready the operating system for testing purposes.
         :return: None
         """
-        execute_java_code()
         position_test_files(self.file_name)
 
     def populate_transactions(self) -> None:
@@ -26,16 +24,6 @@ class Main:
         :return: None
         """
         self.transactions = load_transactions(self.file_name)
-
-    def parse_results(self) -> None:
-        """
-        Parse the output.
-        :return: None
-        """
-        parser = Parser(self.file_name)
-        parser.split_report_parts()
-        parser.parse_report()
-        self.parsed_output = parser.output
 
     def initialise_stats(self) -> None:
         """
@@ -50,17 +38,19 @@ class Main:
         }
 
         template: Dict[str, Dict[str, int]] = {
-            "General": inner_template.copy(),
-            "Corporate": inner_template.copy(),
-            "Individual": inner_template.copy(),
-            "Non-Registered": inner_template.copy()
+            "General": copy(inner_template),
+            "Corporate": copy(inner_template),
+            "Individual": copy(inner_template),
+            "Non-Registered": copy(inner_template)
         }
 
         for transaction_queue in self.transactions:
             dates_keys.append(transaction_queue.day)
 
         for date in dates_keys:
-            self.stats[date] = template.copy()
+            if date == "":
+                break
+            self.stats[date] = copy(template)
 
     def ready(self) -> None:
         """
@@ -69,7 +59,6 @@ class Main:
         """
         self.ready_os()
         self.populate_transactions()
-        self.parse_results()
         self.initialise_stats()
 
     @staticmethod
@@ -82,28 +71,18 @@ class Main:
         for file in files:
             delete_file(file)
 
-    def test_queue_equality(self) -> int:
-        """
-        Test if queue inside the program and reported queues are the same
-        :return: number of the line error ocured or none.
-        """
-        transaction_queues: List[TransactionQueue] = self.parsed_output[0]
-        print(self.parsed_output)
-        for i in range(len(self.transactions)):
-            if self.transactions[i] != transaction_queues[i]:
-                return i
-        return 1
-
     def test(self) -> str:
         self.ready()
-        queue_equality = self.test_queue_equality()
+        """
         if queue_equality >= 0:
             return "Mismatch between reported TransactionQueue and" + \
                    f"calculated TransactionQueue at day {queue_equality + 1}"
+        """
         for transaction_queue in self.transactions:
+            if not transaction_queue.queue:
+                break
             current_date: str = transaction_queue.day
-            while len(transaction_queue.queue) != 0:
-                current_transaction: Transaction = transaction_queue.remove()
+            for current_transaction in transaction_queue.queue:
                 type_of: str = current_transaction.type
                 self.stats[current_date]["General"]["Transaction Count"] += 1
                 self.stats[current_date]["General"]["Total Waiting Time"] += current_transaction.waiting_time
@@ -111,18 +90,19 @@ class Main:
                 self.stats[current_date][type_of]["Total Waiting Time"] += current_transaction.waiting_time
             for key in self.stats[current_date]:
                 cur = current_date  # To shorten the lin length below
-                self.stats[cur][key]["Average Waiting Time"] = self.stats[cur][key]["Total Waiting Time"] / \
-                                                               self.stats[current_date][key]["Transaction Count"]
-
-        for date in self.stats:
-            if self.stats[date] != self.parsed_output[1][date]:
-                return f"Test unsuccessful, error occurred at date {date}"
-
-        return "Test Successful"
-
+                try:
+                    self.stats[cur][key]["Average Waiting Time"] = self.stats[cur][key]["Total Waiting Time"] / self.stats[current_date][key]["Transaction Count"]
+                except ZeroDivisionError:
+                    pass
+        generated_result = ""
+        for transaction_queue in self.transactions:
+            generated_result += str(transaction_queue) + "\n"
+        generated_result += "\n\n" + dumps(self.stats, indent=4)
+        return generated_result
 
 if __name__ == "__main__":
     tester = Main("CENG112_HW3_Transactions.txt")
     result = tester.test()
-    Main.finalise(["CENG112_HW3_Transactions.txt", "output.txt"])
-    print(result)
+    Main.finalise(["CENG112_HW3_Transactions.txt"])
+    with open("output.txt", "w") as file:
+        print(result, file= file)
--- integration_testing/simulation/simulation_os.py
+++ integration_testing/simulation/simulation_os.py
@@ -20,16 +20,4 @@ def position_test_files(file_name: str) -> None:
     :param file_name: File holding the transactions
     :return: None.
     """
-    system(f"cp ../{file_name} {file_name} && cp ../bin/output.txt output.txt")
-
-
-def execute_java_code() -> None:
-    """
-    Execute java code to test.
-    :return: None
-    """
-    command_key = {
-        "Windows": "|",
-        "Linux": ">"
-    }
-    system(f"cd ../bin && java BankApp {command_key[systemname()]} output.txt")
+    system(f"cp ../{file_name} {file_name}")
